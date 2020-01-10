066e8e83c10343dcbf60fba0e3a3bea1715dc09e
rath_miguel <rath.miguel@gmail.com>
2020-01-10 13:36:24 +0900

    サービス案内の構成

--- pages/service/index.vue
+++ pages/service/index.vue
@@ -2,9 +2,48 @@
 div
   TheBreadcrumb(v-bind:list="breadcrumbs")
   ThePagetitle(:title="title", :title_en="title_en")
+  .page-main
+    section.section
+      .container
+        section.subsection
+          .subsection-header
+            h2.c-title.c-title_secondary 自動車販売
+          .subsection-main.subsection-main_flex
+            .subsection-photo: img(src="http://placehold.jp/500x400.png")
+            .subsection-content
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
+        section.subsection
+          .subsection-header
+            h2.c-title.c-title_secondary 自動車整備・メンテナンス
+          .subsection-main.subsection-main_flex
+            .subsection-photo: img(src="http://placehold.jp/500x400.png")
+            .subsection-content
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
+        section.subsection
+          .subsection-header
+            h2.c-title.c-title_secondary 自動車保険の取り扱い
+          .subsection-main.subsection-main_flex
+            .subsection-photo: img(src="http://placehold.jp/500x400.png")
+            .subsection-content
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
+              p 説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります説明が入ります
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+.subsection{
+  overflow: hidden;
+}
+
+.subsection-photo{
+  @include media(md-lg){
+    float: left;
+    margin-left: 0;
+    margin-right: 20px;
+  }
+}
 
 </style>
 

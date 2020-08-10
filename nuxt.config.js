053fe59cbf4722a3279eb7a94204bf9aa720ece8
rath_miguel <rath.miguel@gmail.com>
2020-08-10 20:34:51 +0900

    generateの設定の戻し

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,20 +102,20 @@ export default {
      ]
    },
 
-  // generate: {
-  //   interval: 1000,
-  //   routes () {
-  //     return Promise.all([axios.get(`https://www.autorubys.com/news/wp-json/wp/v2/posts?per_page=100`)]).then((data) => {
-  //       const posts = data[0]
-  //       return posts.data.map((post) => {
-  //         return {
-  //           route: '/news/post/' + post.id,
-  //           payload: post
-  //         }
-  //       })
-  //     })
-  //   }
-  // },
+  generate: {
+    interval: 1000,
+    routes () {
+      return Promise.all([axios.get(`https://www.autorubys.com/news/wp-json/wp/v2/posts?per_page=100`)]).then((data) => {
+        const posts = data[0]
+        return posts.data.map((post) => {
+          return {
+            route: '/news/post/' + post.id,
+            payload: post
+          }
+        })
+      })
+    }
+  },
 
   /*
   ** Build configuration

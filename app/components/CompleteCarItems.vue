fa8d18284222a8d7ed3199361f67f9c5aea667d6
rath_miguel <rath.miguel@gmail.com>
2021-11-15 10:51:43 +0900

    add: コンプリートカーページのデザイン

---
+++ app/components/CompleteCarItems.vue
@@ -0,0 +1,351 @@
+<template lang="pug">
+  .tabarea
+    .tab-header
+      ul.tab-list
+        li.active Jungle Green
+        li Ivory
+    .tab-main
+      .tab-block
+        .sliderarea
+          slick(ref="slick" :options="slickOptions")
+            template(v-for="val in items[0].gallery")
+              .tab-photos: img(:src="val" alt="")
+        .content
+          h2.title-primary コンプリート価格
+          .price
+            .price-tab
+              ul.price-tablist
+                li.active JB74
+                li JB64
+            .price-content
+              .price-block
+                .total
+                  dl.total-dl
+                    dt 合計価格
+                    dd
+                      |￥
+                      span.total-num {{ 2628670 | addComma }}
+                  table.price-table
+                    tbody
+                      tr(v-for="val in items[0].parts")
+                        td {{ val.name }}
+                        td ￥{{ val.price | addComma }}
+                  .captiopn
+                    p ※車両、工賃等含む<br>※コンプリートデモカーのホイールとタイヤだけはお好きにカスタムできます。お気軽にお問い合わせください。
+</template>
+
+<style lang="scss" scoped>
+
+$color-primary: #616C48;
+$color-secondary: #BDB17F;
+
+.tab-list{
+  display: flex;
+  color: $color-primary;
+  li{
+    margin-right: 10px;
+    color: $color-primary;
+    background-color: #E4DCBD;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 255px;
+    height: 75px;
+    font-size: 20px;
+    font-weight: 700;
+    cursor: pointer;
+    @include media(sm){
+      font-size: 14px;
+      height: 44px;
+      width: 120px;
+    }
+    &.active{
+      background-color: #fff;
+    }
+  }
+}
+
+.tab-main{
+  padding: 50px;
+  background-color: #fff;
+  @include media(sm){
+    padding: 15px;
+  }
+}
+
+.sliderarea{
+  background-color: #F2EFE5;
+  overflow: hidden;
+  @include media(sm){
+    margin-left: -15px;
+    margin-right: -15px;
+    margin-top: -15px;
+    background-color: lighten(#F2EFE5, 5);
+    height: auto;
+  }
+  img{
+    height: 490px;
+    width: 670px;
+    object-fit: contain;
+    margin-left: 70px;
+    margin-right: 70px;
+    @include media(md-lg){
+      padding-top: 30px;
+      padding-bottom: 30px;
+    }
+    @include media(sm){
+      width: 220px;
+      height: 220px;
+    }
+  }
+}
+
+/deep/ {
+  .slick-slide{
+    outline: none;
+  }
+
+  .slick-arrow{
+    height: 80px;
+    width: 80px;
+    z-index: 100;
+    background-color: #DBD6C4;
+    height: 100%;
+    @include media(sm){
+      width: 40px;
+    }
+    &:before{
+      opacity: 0;
+    }
+    &.slick-next{
+      right: 0;
+      background-image: url('~assets/images/completecar/complete_right.svg');
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-size: 20px;
+      @include media(sm){
+        right: 0;
+        background-size: 10px;
+      }
+    }
+    &.slick-prev{
+      left: 0;
+      background-image: url('~assets/images/completecar/complete_left.svg');
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-size: 20px;
+      @include media(sm){
+        left: 0;
+        background-size: 10px;
+      }
+    }
+  }
+}
+
+.sliderarea{
+  margin-bottom: 50px;
+  @include media(sm){
+    margin-bottom: 30px;
+  }
+}
+
+.title-primary{
+  color: $color-primary;
+  font-weight: 700;
+  font-size: 24px;
+  border: 3px solid $color-primary;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  width: 255px;
+  margin-bottom: 30px;
+  @include media(sm){
+    width: 160px;
+    font-size: 16px;
+    height: 40px;
+  }
+}
+
+.price-tab{
+  margin-bottom: 30px;
+}
+
+.price-tablist{
+  display: flex;
+  border-bottom: 1px solid lighten($color-secondary, 20);
+  li{
+    cursor: pointer;
+    display: inline-flex;
+    min-width: 190px;
+    height: 50px;
+    justify-content: center;
+    align-items: center;
+    background-color: #F2EFE5;
+    color: $color-primary;
+    
+    @include media(sm){
+      min-width: 120px;
+      height: 44px;
+    }
+    &.active{
+      background-color: #DBD6C4;
+    }
+  }
+}
+
+.total-dl{
+  background-color: #F9F9F9;
+  font-weight: 700;
+  color: $color-primary;
+  font-size: 30px;
+  padding: 8px 20px;
+  margin-bottom: 30px;
+  @include media(sm){
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+  dt{
+    display: inline-block;
+  }
+  dd{
+    display: inline-block;
+  }
+}
+
+.price-table{
+  font-size: 14px;
+  width: 100%;
+  border-top: 3px solid $color-primary;
+  border-bottom: 1px solid $color-primary;
+  color: $color-primary;
+  @include media(sm){
+    font-size: 12px;
+  }
+  tr{
+    background-color: #F2EFE5;
+    &:nth-child(even){
+      background-color: #FBFBFB;
+    }
+
+    th, td{
+      padding: 10px 15px;
+      @include media(sp){
+        padding: 6px 10px;
+        display: block;
+      }
+      &:nth-child(2){
+        text-align: right;
+      }
+    }
+  }
+}
+
+.captiopn{
+  font-size: 12px;
+  margin-top: 10px;
+  p{
+    margin-bottom: 0;
+  }
+}
+
+</style>
+
+<script>
+export default {
+  data: function(){
+    return {
+      curtrent: 1,
+      slickOptions: {
+        dots: false,
+        speed: 500,
+        slidesToShow: 1,
+        variableWidth: true,
+        centerMode: true,
+        autoplay: false,
+        infinite: true,
+        controls: true
+      },
+      items: [
+        {
+          title: 'Jungle Green',
+          gallery: [
+            '/completecar/image4.png',
+            '/completecar/image1.png'
+          ],
+          total: 2628670,
+          parts: [
+            {
+              name: '筆記体クラシック2トーン塗装納品スズスポグリル',
+              price: 64680
+            },
+            {
+              name: 'JB74オールドスタイルバンバーフォグランプユニット付き',
+              price: 65780
+            },
+            {
+              name: 'スチール製マットプラック塗装　クラシックストレートリアパンバーステップ付き',
+              price: 60280
+            },
+            {
+              name: 'タイヤTOYOオーブンカントリーMT225/7SR1×4',
+              price: 72600
+            },
+            {
+              name: 'jb74ホイール　ディーン　16インチ6J-SPCD139.4',
+              price: 104720
+            },
+            {
+              name: 'JB74専用1インチリフトアップキットコイルリアショック延長　プラケットリア用ロングプレーキホース付き',
+              price: 42900
+            },
+            {
+              name: 'その他シートカバーアウトクラスカーズ',
+              price: 27500
+            },
+            {
+              name: 'スベアタイヤカバー',
+              price: 8800
+            },
+            {
+              name: 'ナンバープレート移動キットLEDナンパー灯付き',
+              price: 15180
+            },
+            {
+              name: 'JB74 SZIGENマフラーショートバンバー用',
+              price: 84150
+            },
+            {
+              name: 'スベアタイヤ移動プラケットキット',
+              price: 19580
+            },
+            {
+              name: 'ラプターカラー塗装リア上部、リア側面上部',
+              price: 187000
+            },
+            {
+              name: '工賃JB741インチリフトアッブ',
+              price: 38500
+            },
+            {
+              name: 'JB74新車車両価格',
+              price: 1793000
+            }
+          ]
+        },
+        {
+          title: 'Ivory',
+          gallery: [
+            'image4.png',
+            'image4.png'
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- app/pages/completecar/index.vue
+++ app/pages/completecar/index.vue
@@ -1,144 +1,348 @@
 <template lang="pug">
 div
   TheBreadcrumb(v-bind:list="breadcrumbs")
-  ThePagetitle(:title="title", :title_en="title_en")
   .page-main
-    section.section.section-page
+    .hero
+      .hero-wrap
+        .hero-maincar: img(src="~/assets/images/completecar/hero_car.png" alt="")
+        .hero-content
+          p.hero-lead.hero-lead--en complete my style 
+            strong jimny 
+            |with autobuy's
+          p.hero-lead.hero-lead--ja - 自分だけのジムニー作り -
+    section.section
+      header.section-header
+        h2.title.section-title--ja ラインアップ
+        span.title.section-title--en Model Lineup
+      .bg.bg-primary
+        .container
+          CompleteCarItems
+    section.section
+      header.section-header
+        h2.title.section-title--ja コンプリートカーを選ぶ理由
+        span.title.section-title--en Reason
+      .bg.bg-secondary
+        .container
+          .reason-wrap
+            .reason-block
+              .reason-photo: img(src="~/assets/images/completecar/complete_photo1.jpg" alt="")
+              .reason-content
+                p.lead.lead-reason 新車、中古車納車時と<br class="hide-sm">同時に
+                  span.marker 理想のカスタム
+                  |がされたジムニーに乗れる<br class="hide-sm">素敵プラン
+            .reason-block
+              .reason-photo: img(src="~/assets/images/completecar/complete_photo2.jpg" alt="")
+              .reason-content
+                p.lead.lead-reason ローンの場合はパーツ代、<br class="hide-sm">（工賃代金）を
+                  span.marker 車両代金に含める
+                  |ことができるの
+                  span.marker お得で楽
+            .reason-block
+              .reason-photo: img(src="~/assets/images/completecar/complete_photo3.jpg" alt="")
+              .reason-content
+                p.lead.lead-reason 構造変更が必要なカスタムの<br class="hide-sm">場合、
+                  span.marker 納車時前に手続きが<br class="hide-sm">できる
+                  |のでスムーズ
+            .reason-block
+              .reason-photo: img(src="~/assets/images/completecar/complete_photo4.jpg" alt="")
+              .reason-content
+                p.lead.lead-reason
+                  span.marker ジムニーに特化した
+                  |専門店<br class="hide-sm">だから、知識がなくても<br class="hide-sm">
+                  span.marker お客様の好みや希望通り
+                  |の<br>コンプリートカーが可能
+    section.section.cta
       .container
-        .image-main: img(src="https://placehold.jp/16/b3b3b3/000000/1000x300.png?text=%E3%83%A1%E3%82%A4%E3%83%B3%E3%82%A4%E3%83%A1%E3%83%BC%E3%82%B8%0A" alt="")
-        h3.c-title 納車されたその日からあなただけの特別なクルマに。<br>オートルビーズから、新しい車の買い方の提案です。
-        h2.c-title_info コンプリートカーとは
-        p 市販車を基盤に、内装や外装をカスタマイズした状態でお客様へお渡しする車をコンプリートカーと言います。
-        p オートルビーズでは登録前の新車をベースに、弊社またはお客様がチョイスしたパーツを予め装着して、納車いたします。
-    section.section.section-page
-      .container
-        h2.c-title_info カスタマイズ例
-        .car-main
-          .car-content
-            .car-photo: img(src="https://placehold.jp/40/b3b3b3/000000/1000x500.png?text=%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%82%AB%E3%83%BC%E3%80%80%E3%83%A1%E3%82%A4%E3%83%B3%E5%86%99%E7%9C%9F%0A%0A" alt="")
-            h3.car-title auto ruby's COMPLETE MODEL
-            .car-price
-              .car-price__header
-                h3.car-price__title コンプリート車両本体価格
-              .car-price__content
-                dl.car-price__list
-                  dt JB74
-                  dd ￥2,640,990～
-                    small （税込）
-                dl.car-price__list
-                  dt JB64
-                  dd ￥2,308,130～
-                    small （税込）
-            .car-gallery
-              CarGallery
-            .car-partslist
-              - let i = 1;
-              while i <= 10
-                dl.car-partslist__list
-                  dt スズスポグリル　2トーン　筆記体エンブレム付き
-                  dd ￥64,680
-                - i++
-    section.section.section-page
-      .container
-        h2.c-title_info 購入までの流れ
-        h3.c-title.c-title_secondary 1. ベース車両、及びカスタム内容の決定
-        p まずはメールまたはお電話で、お気軽にお問い合わせください。
-        h3.c-title.c-title_secondary 2. 売買契約の締結
-        p コンプリートカーの仕様と、お見積もり条件など、すべてご納得頂ければ正式契約となります。遠方のお客様は、メールや郵送にて対応させて頂いております。<br>マイカーローンのお取扱いももちろん用意しています。
-        h3.c-title.c-title_secondary 3. ベース車両の仕入れ
-        p お客様のオーダーに合わせて、ベース車両をご用意いたします。
-        h3.c-title.c-title_secondary 4. 各種登録業務
-        p 必要書類をご用意して下さい。難しい書類はございません。基本的には、印鑑証明書をご用意頂く程度です。<br>難しい登録業務に関しましては、こちらで対応させて頂きますのでご安心下さい。
-        h3.c-title.c-title_secondary 5. 車両のカスタム制作
-        p お車が到着いたしましたら、カスタム製作に入らせて頂きます。新車の場合はメーカー・ディーラーオプション以外は装着されていない、フルノーマル状態で到着いたします。<br>塗装からアライメントなどの工程を、すべて自社工場・スタッフにて仕上げていきます。
-        h3.c-title.c-title_secondary 6. 各種チェック・納車準備
-        p 内外装クリーニング・不具合箇所のチェックを行い、お車が完成となります。
-        h3.c-title.c-title_secondary 7. 納車
-        p お車の基本的な操作方法やナビゲーションなどのオーディオ類、カスタムパーツにいたるまで、詳細なご説明をさせて頂きます。
-    section.section.section-page
-      .container
-        .cta
-          h2.c-title.title まずはお気軽にお問い合わせください
-          a(href="").button.c-button.c-button_primary2 お電話でお問い合わせ
-          nuxt-link(to="/contact/").button.c-button.c-button_primary2 お問い合わせフォームへ
+        .cta-panel
+          .cta-header
+            h2.title.cta-title まずはお気軽に<br class="hide-lg">お問い合わせください
+          .cta-content
+            ul.cta-list
+              li: a(href="").tel.c-button.button.button-primary
+                i.icon.icon-phone: img(src="~/assets/images/icons/fa-phone.svg" alt="tel:0544265508")
+                |0544-26-5508
+              li: nuxt-link(to="/contact/").c-button.button.button-seconary メールでお問い合わせ
 </template>
 
 <style lang="scss" scoped>
-.image-main{
-  margin-bottom: 3em;
+
+$color-primary: #616C48;
+$color-secondary: #BDB17F;
+
+//
+// hero
+//
+
+.hero{
+  background-image: url('~/assets/images/completecar/hero.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding-top: 80px;
+  padding-bottom: 80px;
+  text-align: center;
+  color: #FFF;
+  @include media(sm){
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
 }
 
-// 
-// car
-// 
+.hero-maincar{
+  @include media(sm){
+    text-align: center;
+    img{
+      width: 260px;
+    }
+  }
+}
 
-.car-title{
-  font-size: 1.2em;
+.hero-lead{
+  &--en{
+    text-shadow: 1px 1px 0 rgba(#000, 0.5);
+    font-size: 70px;
+    line-height: 1.2;
+    margin-bottom: 50px;
+    font-family: serif;
+    font-family: 'Yellowtail', cursive;
+    @include media(sm){
+      font-size: 30px;
+      margin-bottom: 20px;
+    }
+    strong{
+      font-weight: 500;
+      color: #616C48;
+    }
+  }
+  &--ja{
+    font-size: 32px;
+    text-shadow: 1px 1px 0 rgba(#000, 0.5);
+    line-height: 1.2;
+    margin-bottom: 0;
+    font-weight: 700;
+    @include media(sm){
+      font-size: 20px;
+    }
+  }
+}
+
+// bg
+
+.bg{
+  padding-top: 80px;
+  padding-bottom: 80px;
+  @include media(pc){
+    padding-top: 100px;
+    padding-bottom: 150px;
+  }
+}
+
+.bg-primary{
+  background-color: #F2EFE5;
+}
+
+.bg-secondary{
+  background-color: #F8F7F2;
+  @include media(sm){
+    padding-top: 50px;
+    padding-bottom: 10px;
+  }
+}
+
+// section
+
+.section{
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+
+.section-header{
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  border-top: 2px solid $color-primary;
+  border-bottom: 2px solid $color-primary;
+  padding-top: 35px;
+  padding-bottom: 35px;
+  margin-bottom: 100px;
+  margin-top: 20px;
+  color: $color-primary;
+  @include media(sm){
+    margin-left: 20px;
+    margin-right: 20px;
+    padding-top: 25px;
+    padding-bottom: 25px;
+    margin-bottom: 50px;
+  }
+  .title{
+    margin-bottom: 0;
+    line-height: 1;
+    display: block;
+  }
+}
+
+.section-title{
+  &--ja{
+    font-size: 34px;
+    font-weight: 700;
+    margin-bottom: 15px;
+    @include media(sm){
+      font-size: 22px;
+    }
+  }
+  &--en{
+    font-size: 16px;
+    margin-top: 15px;
+    @include media(sm){
+      font-size: 12px;
+      margin-top: 10px;
+    }
+  }
+}
+
+// reason
+
+.reason-block{
+  display: flex;
+  @include media(sm){
+    flex-direction: column;
+    margin-bottom: 40px;
+  }
+  &:nth-child(even){
+    @include media(md-lg){
+      flex-direction: row-reverse;
+    }
+  }
+}
+
+.reason-photo{
+  @include media(md-lg){
+    width: 100%;
+  }
+  @include media(sm){
+    margin-left: -15px;
+    margin-right: -15px;
+  }
+  img{
+    display: block;
+    width: 100%;
+    @include media(sm){
+      aspect-ratio: 16 / 9;
+      object-fit: cover;
+    }
+  }
+}
+
+.reason-content{
+  width: calc(100% - 40px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  padding: 20px;
+  @include media(sm){
+    padding: 20px 0;
+    width: auto;
+  }
+}
+
+.lead-reason{
+  font-size: 35px;
+  color: $color-primary;
+  margin-bottom: 0;
   font-weight: 700;
-  margin-bottom: 0.5em;
-  border-bottom: 1px solid #CCC;
-  padding-bottom: 5px;
-}
-
-.car-price{
-  display: flex;
-  justify-content: flex-end;
-  font-size: 0.9em;
-
-  &__header{
-    margin-right: 1em;
+  @include media(sm){
+    font-size: 20px;
   }
-
-  &__list{
-    display: table;
-    margin-bottom: 0.2em;
-    dt{
-      display: table-cell;
-    }
-    dd{
-      display: table-cell;
-    }
-  }
-
-  small{
-    font-size: 0.86em;
+  .marker{
+    background:linear-gradient(transparent 65%, #FCE300 65%);
   }
 }
 
-.car-partslist{
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 2em;
-  font-size: 0.9em;
-  
-  &__list{
-    box-sizing: border-box;
-    border-bottom: 1px solid #CCC;
-    padding: 0.5em 0.75em;
-    margin-left: 10px;
-    margin-right: 10px;
-    width: calc(50% - 20px);
-    margin-bottom: 0.25em;
-    padding-bottom: 0.25em;
-    display: flex;
-    dd{
-      margin-left: auto;
-      padding-left: 1em;
-    }
-  }
-}
+// cta
 
 .cta{
-  text-align: center;
+  margin-bottom: 150px;
+  @include media(sm){
+    margin-bottom: 80px;
+  }
+}
 
-  .title{
-    font-size: 1.6em;
+.cta-panel{
+  border: 4px solid $color-primary;
+  padding: 70px 60px;
+  padding-top: 50px;
+  @include media(sm){
+    padding: 10px;
+    border-width: 2px;
+  }
+}
+
+.cta-title{
+  text-align: center;
+  margin-bottom: 30px;
+  color: $color-primary;
+  font-weight: 700;
+  font-size: 30px;
+  @include media(sm){
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+}
+
+.cta-list{
+  display: flex;
+  justify-content: center;
+  @include media(sm){
+    flex-direction: column;
+    align-items: center;
+  }
+  li{
+    margin-left: 10px;
+    margin-right: 10px;
+    @include media(sm){
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
   }
 
   .button{
-    margin-left: 5px;
-    margin-right: 5px;
-    margin-top: 1em;
+    width: 320px;
+    height: 85px;
+    font-size: 20px;
+    font-weight: 700;
+    border: 2px solid $color-primary;
+    border-radius: 100px;
+    @include media(sm){
+      width: 100%;
+      font-size: 16px;
+      height: 60px;
+      width: 260px;
+    }
+    &:hover{
+      opacity: 0.8;
+    }
+  }
+
+  .icon-phone{
+    display: inline-block;
+    margin-right: 10px;
+    width: 28px;
+    @include media(sm){
+      width: 20px;
+    }
+  }
+
+  .button-primary{
+    background-color: $color-primary;
+    color: #FFF;
+    font-size: 28px;
+    @include media(sm){
+      font-size: 22px;
+    }
+  }
+
+  .button-seconary{
+    color: $color-primary;
   }
 }
 
@@ -146,19 +350,17 @@ div
 
 <script>
 
-import ThePagetitle from '~/components/ThePagetitle.vue'
 import TheBreadcrumb from '~/components/TheBreadcrumb.vue'
-import CarGallery from '~/components/CarGallery.vue'
+import CompleteCarItems from '~/components/CompleteCarItems.vue'
 
 export default {
   components: {
-    ThePagetitle,
     TheBreadcrumb,
-    CarGallery
+    CompleteCarItems
   },
   head () {
     return {
-      title: 'コンプリートカー - 株式会社オートルビーズ（鳥取県 岩美町）'
+      title: 'コンプリートカー - 株式会社オートルビーズ'
     }
   },
   data: function() {

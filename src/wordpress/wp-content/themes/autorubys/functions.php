546b3414eb551e26f7050dc6eca264ba1b9ddb8e
rath_miguel <rath.miguel@gmail.com>
2022-06-01 19:18:46 +0900

    add: プレビュー時のリダイレクト先を設定

--- src/wordpress/wp-content/themes/autorubys/functions.php
+++ src/wordpress/wp-content/themes/autorubys/functions.php
@@ -4,8 +4,8 @@ add_theme_support( 'post-thumbnails' );
 
 // 管理画面用のスタイルの挿入
 function my_admin_enqueue($hook) {
-  wp_enqueue_style( 'my_admin_css', get_template_directory_uri() . '/admin.css' );
-  wp_enqueue_script( 'my_admin_script', get_template_directory_uri() . '/admin.js' );
+  // wp_enqueue_style( 'my_admin_css', get_template_directory_uri() . '/admin.css' );
+  // wp_enqueue_script( 'my_admin_script', get_template_directory_uri() . '/admin.js' );
 }
 add_action( 'admin_enqueue_scripts', 'my_admin_enqueue' );
 
@@ -58,19 +58,45 @@ register_taxonomy(
   )
 );
 
-// 不要なメニューの削除
+add_action("template_redirect", function () {
+  $home_url = 'https://autorubys.com/';
+  if (is_home()) {
+    wp_redirect( $home_url );
+    exit;
+  }
 
+  if (is_singular('catalog') && !is_preview()) {
+    wp_redirect( $home_url.'/products/item/'.get_the_ID() );
+    exit;
+  }
+
+  if (is_single() && !is_preview()) {
+    wp_redirect( $home_url.'/news/post/'.get_the_ID() );
+    exit;
+  }
+
+  if (!is_admin() && isset($_GET['preview']) && $_GET['preview'] == true) {
+    $redirect = add_query_arg(
+      [
+        'id' => $_GET['preview_id'] ? $_GET['preview_id'] : $_GET['p'],
+        'nonce' => wp_create_nonce( 'wp_rest' )
+      ],
+      $home_url.'/news/post/preview'
+    );
+  wp_redirect($redirect);
+  exit;
+  }
+});
+
+// 不要なメニューの削除
 function remove_menus () {
   global $menu;
- 
   remove_menu_page('edit-comments.php');
 }
 add_action('admin_menu', 'remove_menus', 99);
  
-   function remove_wp_nodes()
-{
+function remove_wp_nodes(){
   global $wp_admin_bar;
- 
   $wp_admin_bar->remove_node( 'comments' );
 }
 add_action('admin_bar_menu', 'remove_wp_nodes', 99);

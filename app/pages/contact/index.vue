81025058c8060091c2b2c0def09967249d31e0f6
rath_miguel <rath.miguel@gmail.com>
2022-06-30 18:31:25 +0900

    change: 定休日の変更

--- app/pages/contact/index.vue
+++ app/pages/contact/index.vue
@@ -28,7 +28,7 @@ div
             dl.tel-dl.tel-dl--primary
               span.tel-title tel.
               span.tel-text: a(href="tel:0857720727").tel 0857-72-0727
-            .touch-worktime: small 受付時間：10：00～19：00 日曜定休
+            .touch-worktime: small 受付時間：9：00～18：00 <br>日曜日、第一水曜日、第二水曜日定休
             p E-MAIL：&#114;&#97;&#110;&#99;&#105;&#100;&#54;&#57;&#54;&#106;&#112;&#64;&#97;u&#116;o-r&#117;&#98;&#121;&#115;&#46;&#99;&#111;m
             p: nuxt-link(v-scroll-to="'#contact-form'" to) フォームからお問い合わせ
           .touch-panel
